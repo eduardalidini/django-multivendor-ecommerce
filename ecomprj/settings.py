f4de66759b8026cfbcc8529efb14fb55b64dcc21
aurelqoku777 <aurel.qoku@fshnstudent.info>
2026-01-02 18:00:10 +0100

    Custom Django Admin using Jazzmin

--- ecomprj/settings.py
+++ ecomprj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',              
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +133,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    "site_header": "FoodMart",
+    "site_brand": "FoodMart",
+    "site_logo": "assets/images/food-mart-animated-led-sign.gif", 
+    "login_logo": "assets/images/food-mart-animated-led-sign.gif",
+    "copyright": "FoodMart.com",
+}
